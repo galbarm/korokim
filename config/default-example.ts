9c1b3359c082c17efa88468c060a4270a26e4d6d
galbarm <galbarm@gmail.com>
2023-07-07 12:47:11 +0300

    add example config

---
+++ config/default-example.ts
@@ -0,0 +1,39 @@
+//fill and rename to default.ts
+
+import { CompanyTypes } from 'israeli-bank-scrapers';
+
+export default {
+    mongoUrl: "",
+
+    sendGridAPIKey: "",
+
+    friendlyNames: {
+        "1111": "max",
+        "2222": "הפועלים"
+    },
+
+    startDate: "2023-07-05",
+
+    sender: "your_verified_sendgrid@email.address",
+    targets: ["target@gmail.com"],
+
+    updateIntervalMin: 60,
+
+    accounts: [
+        {
+            company: CompanyTypes.visaCal,
+            username: "username1",
+            password: "password1"
+        },
+        {
+            company: CompanyTypes.max,
+            username: "username2",
+            password: "password2"
+        },
+         {
+            company: CompanyTypes.hapoalim,
+            userCode: "username3",
+            password: "password3"
+        },
+    ],
+}
