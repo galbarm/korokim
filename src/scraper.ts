b835416cf15e9d152d9a3d872e671265c7141f2f
galbarm <galbarm@gmail.com>
2026-06-07 21:57:34 +0300

    apply --no-sandbox only in CI

--- src/scraper.ts
+++ src/scraper.ts
@@ -11,7 +11,7 @@ export async function scrape(account: any, from: Date): Promise<ScraperScrapingR
     showBrowser: false,
     timeout: 120000,
     defaultTimeout: 120000,
-    args: ['--no-sandbox', '--disable-setuid-sandbox'],
+    args: process.env.CI ? ['--no-sandbox', '--disable-setuid-sandbox'] : [],
   }
 
   const credentials = {
